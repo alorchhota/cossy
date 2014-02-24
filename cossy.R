fc5ad97429eea8b7c3ea3d07b4b096cea044dd98
ashis <alorchhota@gmail.com>
2014-02-24 18:25:10 +0900

    defiend the methods of the package.

---
+++ cossy.R
@@ -0,0 +1,23 @@
+readExpression <- function(gctfile, chipfile=NA){
+  
+}
+
+readClass <- function(clsfile){
+  
+}
+
+preprocessTrainingExpression <- function(expression, qnorm=T, ztrans=T){
+  
+}
+
+preprocessTestExpression <- function(preprocessObj, expression){
+  
+}
+
+cossy <- function(expression, cls, misset, nmis=5){
+  
+}
+
+predict <- function(cossyobj, expression){
+  
+}
