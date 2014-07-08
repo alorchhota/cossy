7efe1e4e8c9ff2755c9541e8ba9f992d12c15831
ashis <alorchhota@gmail.com>
2014-07-08 16:40:11 +0900

    icossy build script updated.

--- packages/build-icossy.sh
+++ packages/build-icossy.sh
@@ -2,6 +2,8 @@
 ##### commands to create icossy package #####
 #############################################
 
+mkdir data
+
 cp ./../icossy/icossy.R ./icossy/R/icossy.R
 cp ./../data/pathwayapi.rda ./icossy/data/pathwayapi.rda
 cp ./../data/pathwayapi_json.rda ./icossy/data/pathwayapi_json.rda
