f17080bc588aeb02a158adff6f1f46cdba49e5d5
ashis <alorchhota@gmail.com>
2014-07-08 16:25:32 +0900

    build script for icossy modified. Now data is copied to the package folder.

--- packages/build-icossy.sh
+++ packages/build-icossy.sh
@@ -3,5 +3,8 @@
 #############################################
 
 cp ./../icossy/icossy.R ./icossy/R/icossy.R
+cp ./../data/pathwayapi.rda ./icossy/data/pathwayapi.rda
+cp ./../data/pathwayapi_json.rda ./icossy/data/pathwayapi_json.rda
+
 R CMD build icossy
 R CMD check icossy
